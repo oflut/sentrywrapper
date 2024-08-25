1afa6be8662a18f459681184e4f576102e42b995
mertcanereren <mert.eren@rapsodo.com>
2024-08-25 17:50:59 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module sentrywrapper
+
+go 1.22.5
+
+require (
+	github.com/getsentry/sentry-go v0.28.1
+	go.etcd.io/bbolt v1.3.11
+)
+
+require (
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ sentrywrapper.go
@@ -0,0 +1,64 @@
+package sentrywrapper
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+)
+
+func New(dsn string, options ...Option) *Wrapper {
+	w := &Wrapper{
+		dsn:              dsn,
+		environment:      "production",
+		sampleRate:       1.0,
+		maxBreadcrumbs:   100,
+		attachStacktrace: true,
+	}
+
+	for _, option := range options {
+		option(w)
+	}
+
+	return w
+}
+
+func (w *Wrapper) Initialize() error {
+	return sentry.Init(sentry.ClientOptions{
+		Dsn:              w.dsn,
+		Environment:      w.environment,
+		Release:          w.release,
+		Debug:            w.debug,
+		SampleRate:       w.sampleRate,
+		MaxBreadcrumbs:   w.maxBreadcrumbs,
+		AttachStacktrace: w.attachStacktrace,
+	})
+}
+
+func (w *Wrapper) CaptureException(err error) {
+	sentry.CaptureException(err)
+}
+
+func (w *Wrapper) CaptureMessage(message string) {
+	sentry.CaptureMessage(message)
+}
+
+func (w *Wrapper) AddBreadcrumb(breadcrumb *sentry.Breadcrumb) {
+	sentry.AddBreadcrumb(breadcrumb)
+}
+
+func (w *Wrapper) Flush(timeout time.Duration) bool {
+	return sentry.Flush(timeout)
+}
+
+func (w *Wrapper) WithContext(ctx context.Context) context.Context {
+	return sentry.SetHubOnContext(ctx, sentry.CurrentHub().Clone())
+}
+
+func (w *Wrapper) Recover() {
+	if err := recover(); err != nil {
+		w.CaptureException(err.(error))
+		log.Printf("Recovered from panic: %v", err)
+	}
+}
